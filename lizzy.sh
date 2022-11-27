00947b864028484d2b39d62882c63caff69b72d9
lgoldman22 <116844510+lgoldman22@users.noreply.github.com>
2022-11-27 13:27:27 -0800

    Add files via upload

---
+++ lizzy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Luke Goldman, 11/8/2022
+
+#Input must be in quotations. For example, './lizzy.sh "lizzy Lizzy izzy hizzy hello"' will return '2'
+#This will search the input for every instance of lizzy and output the amount of instances of lizzy
+echo $1 | grep [Ll]izzy -o | wc -w
